66aa6dfd628a5927a997a891592caa122fbe1a7a
anantadwi13 <anantadwi13@yahoo.com>
2020-04-25 16:38:58 +0700

    update test view

--- app/common/controllers/ErrorController.php
+++ app/common/controllers/ErrorController.php
@@ -9,12 +9,18 @@ class ErrorController extends Controller
 {
     public function initialize()
     {
-        $this->view->setViewsDir(__DIR__ . '/../views');
+        $this->view->setViewsDir(__DIR__ . '/../views/');
+        $this->view->setLayoutsDir(__DIR__ . '/../layouts/');
+        $this->view->setPartialsDir(__DIR__ . '/../another/');
     }
 
     public function notFoundAction()
     {
-        $this->view->setLayoutsDir(__DIR__ . '/../views');
+        $this->view->setTemplateBefore('main');
+        $this->view->setLayout('coba_layout');
+        $this->view->setTemplateAfter('coba_layout_2');
+//        $this->view->partial('another_layout');
+//        $this->view->pick('common/myview');
         echo '404 - not found';
     }
 
