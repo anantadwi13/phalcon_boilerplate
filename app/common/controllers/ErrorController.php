8fbaa448b7d91af7e15c3eb52312d2ff4dbc8d9f
anantadwi13 <anantadwi13@yahoo.com>
2020-04-25 17:05:39 +0700

    refactor directory

--- app/common/controllers/ErrorController.php
+++ app/common/controllers/ErrorController.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 namespace App\Common\Controllers;
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Mvc\View;
 
 class ErrorController extends Controller
 {
     public function initialize()
     {
-        $this->view->setViewsDir(__DIR__ . '/../views/');
-        $this->view->setLayoutsDir(__DIR__ . '/../layouts/');
-        $this->view->setPartialsDir(__DIR__ . '/../another/');
+        $this->view->setViewsDir(__DIR__ . '/../resources/views/');
+        $this->view->setLayoutsDir(__DIR__ . '/../resources/layouts/');
+        $this->view->setPartialsDir(__DIR__ . '/../resources/partials/');
+        // Disable MAIN Layout View, Because it is confusing (anti pattern)
+        $this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
     }
 
     public function notFoundAction()
@@ -19,7 +22,7 @@ class ErrorController extends Controller
         $this->view->setTemplateBefore('main');
         $this->view->setLayout('coba_layout');
         $this->view->setTemplateAfter('coba_layout_2');
-//        $this->view->partial('another_layout');
+        $this->view->partial('partial_view');
 //        $this->view->pick('common/myview');
         echo '404 - not found';
     }
